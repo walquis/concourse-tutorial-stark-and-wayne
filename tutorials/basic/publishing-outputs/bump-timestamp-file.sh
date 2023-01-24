fb55213caf3c58008bf84fbe3ce3b459db8f3c72
Chris Walquist <cwalquist@drw.com>
2023-01-24 16:45:55 -0600

    add safe.directory to git config

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -10,6 +10,7 @@ date > bumpme
 
 git config --global user.email "nobody@concourse-ci.org"
 git config --global user.name "Concourse"
+git config --global safe.directory "*"
 
 git add .
 git commit -m "Bumped date"
